d94ea5f27724d11f5538f85d3d64c95a55da1953
ulldecorb <ausias.bertran@gmail.com>
2022-06-02 15:38:43 +0200

    Create html on home component

--- cv/src/components/home/index.jsx
+++ cv/src/components/home/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './home.css';
 
 function Home() {
   return (
-    <div>Home</div>
+    <main className="home">
+      <navbar className="home__navbar">
+        <div>Home</div>
+        <Link to="/pdf">pdf</Link>
+        <Link to="/formal">Formal</Link>
+        <Link to="/animated">animated</Link>
+      </navbar>
+      <section className="home__profile-box">
+        <article className="profile-box__image">
+          photo
+        </article>
+        <article className="profile-box__about">
+          jelou
+        </article>
+        <article className="profile-box__about" />
+        <article className="profile-box__languages" />
+        <article className="profile-box__work" />
+        <article className="profile-box__studies" />
+        <article className="profile-box__skills" />
+
+      </section>
+    </main>
   );
 }
 
