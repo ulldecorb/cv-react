939547808eacc30429fe43ff6acf6994f83d9823
ulldecorb <ausias.bertran@gmail.com>
2022-06-28 13:07:37 +0200

    Start style on minimal component

--- cv/src/components/Minimal/index.jsx
+++ cv/src/components/Minimal/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { showProfile, hideProfiles } from '../../scripts/index';
 import './minimal.css';
@@ -7,15 +7,15 @@ function Minimal({ cv }) {
   return (
     <main>
 
-      <section className="minimal__control-box">
-        <nav className="minimal-control-box__navbar-box">
-          <div id="about-handler" className="minimal-control-box__navbar-title" onMouseEnter={(e) => showProfile(e)}>AUSIAS BERTRAN</div>
-          <div id="skills-handler" className="minimal-control-box__navbar-title" onMouseEnter={(e) => showProfile(e)}>SKILLS</div>
-          <div id="work-handler" className="minimal-control-box__navbar-title" onMouseEnter={(e) => showProfile(e)}>WORK EXPERIENCE</div>
-          <div id="languages-handler" className="minimal-control-box__navbar-title" onMouseEnter={(e) => showProfile(e)}>LANGUAGES</div>
-          <div id="education-handler" className="minimal-control-box__navbar-title" onMouseEnter={(e) => showProfile(e)}>EDUCATION</div>
-        </nav>
-      </section>
+      <nav className="minimal__control-box">
+        {/* <nav className="minimal-control-box__navbar-box"> */}
+        <div id="about-handler" className="control-box__navbar-title" onMouseEnter={(e) => showProfile(e)}>AUSIAS BERTRAN</div>
+        <div id="skills-handler" className="control-box__navbar-title" onMouseEnter={(e) => showProfile(e)}>SKILLS</div>
+        <div id="work-handler" className="control-box__navbar-title" onMouseEnter={(e) => showProfile(e)}>WORK EXPERIENCE</div>
+        <div id="languages-handler" className="control-box__navbar-title" onMouseEnter={(e) => showProfile(e)}>LANGUAGES</div>
+        <div id="education-handler" className="control-box__navbar-title" onMouseEnter={(e) => showProfile(e)}>EDUCATION</div>
+        {/* </nav> */}
+      </nav>
 
       <section id="profile" className="minimal__profile-box" onMouseLeave={(e) => hideProfiles(e)}>
         <article id="about" className="profile-box__article">{cv.aboutMe.description}</article>
@@ -23,14 +23,11 @@ function Minimal({ cv }) {
         <article id="work" className="profile-box__article">{cv.workExperience[0].article}</article>
         <article id="languages" className="profile-box__article profile-box__languages">
           {cv.languages.map((language) => (
-            <Fragment key={`minimal${language.language}`}>
-              <h3>
-                {language.language}
-                {': '}
-                {language.level}
-              </h3>
-              <br />
-            </Fragment>
+            <h3 key={`minimal${language.language}`} className="languages">
+              {language.language}
+              {': '}
+              {language.level}
+            </h3>
           ))}
         </article>
         <article id="education" className="profile-box__article">{cv.education[0].article}</article>
